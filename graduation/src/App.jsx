517dcbcc0cdbbfa249711601baf38850521a7ab8
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-04-17 19:54:39 +0700

    Fix conflic

--- graduation/src/App.jsx
+++ graduation/src/App.jsx
@@ -4,28 +4,51 @@ import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./components/Layout";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { getCourseType } from "./api";
+import { useStateValue } from "./Context/StateProvider";
+import { actionType } from "./Context/reducer";
 
-import Main from "./components/Main"
+import Main from "./components/Main";
 import Course from "./components/Courses/Course";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Login from "./components/AuthUser/Login/Login";
+import Register from "./components/AuthUser/Register/Register";
 import Instructor from "./components/instructor/Instructor";
 import Cart from "./components/Cart";
-import Dashboard from "./components/Dashboard/Dashboard";
+import NewCourse from "./components/CourseDetails/NewCourse";
+import Lesson from "./components/Lesson";
 
 const publicRoutes = [
   { path: "/", component: Main },
   { path: "/course", component: Course },
   { path: "/instructor", component: Instructor },
 
-  { path: "/login", component: Login, layout:null},
-  { path: "/register", component: Register, layout:null },
+  { path: "/login", component: Login, layout: null },
+  { path: "/register", component: Register, layout: null },
   { path: "/cart", component: Cart },
-  { path: "/dashboard", component: Dashboard },
+
+  { path: "/newcourse", component: NewCourse },
+  { path: "/lesson/:id", component: Lesson },
 ];
 
 function App() {
+  const [{ courseType }, dispatch] = useStateValue();
+  useEffect(() => {
+    fetchCourseType();
+  }, []);
+  const fetchCourseType = async () => {
+    try {
+      const res = await getCourseType(); // Gọi API
+      // console.log(res.data.data);
+      dispatch({
+        type: actionType.SET_TYPECOURSE,
+        typeCourse: res.data.data,
+      });
+    } catch (error) {
+      // console.error("Error fetching course type:", error);
+    }
+  };
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
