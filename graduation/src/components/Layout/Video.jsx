43216171ea756b4c84d00bb5b1cbb21a3289b828
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-05-07 01:36:37 +0700

    hehe

---
+++ graduation/src/components/Layout/Video.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Video() {
+    return (
+        <div>
+            <div className="lesson-section-video">
+                {/* <ReactPlayer
+                    width="100%"
+                    height="100%"
+                    controls={true}
+                    url={dataOneCourse?.data?.course_demoVideo}
+                /> */}
+            </div>
+        </div>
+    )
+}
